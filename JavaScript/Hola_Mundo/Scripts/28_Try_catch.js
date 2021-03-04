c57705b5d4330590289344996a93dafab4a8f60e
Camilo <camilocool352@gmail.com>
2021-03-03 20:08:03 -0500

    Se adjunta trabajo de hoy

---
+++ JavaScript/Hola_Mundo/Scripts/28_Try_catch.js
@@ -0,0 +1,10 @@
+try {
+    var year = '2013';
+    alert(yea);
+    var vector = new Array(999999999999999999999999999);
+    console.log(decodeURIComponent("https://www.facebook.com/"));
+
+} catch (error) {
+    console.log(error);
+    alert("A ocurrido un error en el codigo");
+}
