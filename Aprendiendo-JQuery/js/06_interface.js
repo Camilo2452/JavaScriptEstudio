85966f81e6ba15853438527a7005b8682abaf573
Camilo <camilocool352@gmail.com>
2021-05-29 00:16:48 -0500

    Se sube fuentes para maquetacion futura

--- Aprendiendo-JQuery/js/06_interface.js
+++ Aprendiendo-JQuery/js/06_interface.js
@@ -1,3 +1,62 @@
 $(document).ready(function(){
-    alert("Todo Melo Caramelo");
+    var elemento = $(".elemento");
+    var lista_li = $(".lista-seleccionable");
+    /*************/
+    var elemento_movido = $("#elemento-movido");
+    var area            = $("#area");
+    var mostrar         = $("#mostrar");
+    var caja_efecto     = $(".caja-efecto");
+    var popup           = $("#popup");
+    var lanzar_popup    = $("#lanzar-popup");
+    var calendario      = $("#calendario");
+    var tabs            = $("#pestanas");
+    //Mover Elemento por la Pagina
+    elemento.draggable();
+
+     //Mover Elemento por la Pagina
+     elemento.resizable();
+    
+     //Seleccionar
+     lista_li.selectable();
+
+     //Organizar
+    /**  lista_li.sortable({
+         update: function(event,ui){
+             console.log("Ha cambiado la lista");
+         }
+     });
+*/
+     //Drop
+     elemento_movido.draggable();
+     area.droppable({
+        drop:function(){
+            console.log("Has Soltado Algo Dentro del Area");
+        }
+     });
+
+     //Efectos
+     mostrar.click(function(){
+        caja_efecto.effect("shake",4000);
+        //fadeToggle
+        //efect("fade")
+        //explode
+        //blind
+        //slide
+        //drop
+        //fold
+        //puff
+     });
+     //Tooltip
+     $(document).tooltip();
+
+     //Dialog
+     lanzar_popup.click(function(){
+        popup.dialog();
+     });
+    
+     //DatePicker
+     calendario.datepicker();
+     
+     //tabs
+     tabs.tabs();
 });
